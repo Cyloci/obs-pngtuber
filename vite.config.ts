bd6167af9a97fb2a200ba31cff92fbbfac46575b
Cyloci <me@cyloci.com>
2022-05-29 13:01:35 -0500

    fix deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import svgr from "@honkhonk/vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/obs-pngtuber/",
   plugins: [react(), svgr()],
   server: {
     host: true,
